977fd4036247e2d5f6a50ec1a0cf65084d5b2b24
Dominic Rico-Gomez <hello@coderocket.me>
2016-02-22 12:03:44 +0100

    removed blank lines

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,6 +45,6 @@ module.exports = function(grunt) {
 
   grunt.registerTask('test', ['jshint', 'mochaTest']);
 
-  grunt.registerTask('build', ['default'])
+  grunt.registerTask('build', ['default']);
 
 };
